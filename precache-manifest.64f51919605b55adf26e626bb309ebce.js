d130f826d2aa773adae20f3432ade9c8d1071ea1
Santhosh <bgj.santhosh@gmail.com>
2020-03-04 11:30:13 +0530

    Updates

---
+++ precache-manifest.64f51919605b55adf26e626bb309ebce.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f16c67efb28ec181dd2808b5e0b9aca1",
+    "url": "/productcompare/index.html"
+  },
+  {
+    "revision": "af67e732d7aead5161de",
+    "url": "/productcompare/static/css/main.8fb5841a.chunk.css"
+  },
+  {
+    "revision": "80522641cd30eaabcdda",
+    "url": "/productcompare/static/js/2.9b4d5781.chunk.js"
+  },
+  {
+    "revision": "94821d044d113bbacb2e96d520c19b98",
+    "url": "/productcompare/static/js/2.9b4d5781.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af67e732d7aead5161de",
+    "url": "/productcompare/static/js/main.8c20d482.chunk.js"
+  },
+  {
+    "revision": "bcbaa5f247dae5914385",
+    "url": "/productcompare/static/js/runtime-main.7188c65e.js"
+  },
+  {
+    "revision": "8d5e6dc4e0efa4543954f426cf5bde50",
+    "url": "/productcompare/static/media/Cherry.8d5e6dc4.png"
+  },
+  {
+    "revision": "d52b5106344b5666f1ef8622805e976e",
+    "url": "/productcompare/static/media/Nuts.d52b5106.png"
+  },
+  {
+    "revision": "4c9e0bb7b532c6198cf68f989f1966a8",
+    "url": "/productcompare/static/media/Orange.4c9e0bb7.png"
+  },
+  {
+    "revision": "4a2da8d948571e87b343f07de4a35568",
+    "url": "/productcompare/static/media/Strawberry.4a2da8d9.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/productcompare/static/media/logo.5d5d9eef.svg"
+  }
+]);
